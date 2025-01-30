02ef62c88a4f9a7f8d9e827e39496f3dd31953c9
Talha Bin Faisal <talhafaisal2510@gmail.com>
2025-01-30 13:16:57 +0500

    Resolved merge conflict in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -30,9 +30,6 @@
 
 // export default App;
 
-
-
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/LoginPage";
@@ -41,41 +38,32 @@ import Payment from "./components/payment";
 import Donation from "./components/Donation";
 import Success from "./components/Success";
 // import PredictionForm from "./components/PredictionForm/PredictionForm";
-// import ContactUsForm from "./components/Contact Us/Contact";
-// import Form from "./components/Verification Form/Form";
-// import AboutUs from "./components/About US/AboutUs";
-import HomePage from "./pages/HomePage";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import DonationPage from "./pages/DonationPage"
+// import ContactUsForm from"./components/Contact Us/Contact";
+import { Route, Routes } from "react-router-dom";
+// import Form from "./components/Verification Form/Form"
+// import AboutUs from "./components/About US/AboutUs"
+// import HomePage from "./pages/HomePage";
+import Payment from "./components/payment";
+import FileUpload from "./components/fileUpload";
+import ViewFiles from "./components/DisplayFiles";
 function App() {
   return (
-    <>
-     
-    <Router>
-    
-      {/* <Header /> */}
-      <div className="App">
-        <Routes>
-         
-          {/* <Route path="/" element={<HomePage />} /> */}
-          <Route path="/" element={<DonationPage />} />
-          <Route path="/success" element={<Success />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/payment" element={<Payment />} />
-
-
-          {/* <Route path="/contact" element={<ContactUsForm />} />
-          <Route path="/predict" element={<PredictionForm />} />
-          <Route path="/form" element={<Form />} />
-          <Route path="/about" element={<AboutUs />} /> */}
-        </Routes>
-      </div>
-    
-      {/* <Footer /> */}
-    </Router>
-    </>
+    <div className="App">
+      <Routes>
+        {/* <Route path="/" element={<HomePage />} /> */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/file" element={<FileUpload />} />
+        <Route path="/fileview" element={<ViewFiles />} />
+        {/* <Route path="/contact" element={<ContactUsForm />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/predict" element={<PredictionForm />} />
+        <Route path="/form" element={<Form />} />
+        <Route path="/about" element={<AboutUs />} />
+         */}
+      </Routes>
+    </div>
   );
 }
 
