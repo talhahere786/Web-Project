9abde38f082c89e76d8cb1f91aa75b8a3520a59c
Fatima Javed <fatimajavedd444@gmail.com>
2025-02-09 19:12:24 +0500

    Added About Us

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,3 @@
-// import React from "react";
-// import Login from "./pages/LoginPage";
-// import Signup from "./pages/SignupPage";
-// // import Signup from "./components/Signup/signup";
-// // import PredictionForm from "./components/PredictionForm/PredictionForm";
-// // import ContactUsForm from"./components/Contact Us/Contact";
-// import { Route,Routes } from "react-router-dom";
-// // import Form from "./components/Verification Form/Form"
-// // import AboutUs from "./components/About US/AboutUs"
-// // import HomePage from "./pages/HomePage";
-// import Payment from "./components/payment"
-// function App() {
-//   return (
-//     <div className="App">
-//       <Routes>
-//         {/* <Route path="/" element={<HomePage />} /> */}
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/signup" element={<Signup />} />
-//         <Route path="/payment" element={<Payment />} />
-//         {/* <Route path="/contact" element={<ContactUsForm />} />
-//         <Route path="/signup" element={<Signup />} />
-//         <Route path="/predict" element={<PredictionForm />} />
-//         <Route path="/form" element={<Form />} />
-//         <Route path="/about" element={<AboutUs />} />
-//          */}
-//       </Routes>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/LoginPage";
@@ -51,6 +16,7 @@ import DonationPage from "./pages/DonationPage"
 import Dashboard from "./components/Dashboard";
 import DonarDashboardPage from "./pages/DonarDashboardPage";
 import AboutUs from "./components/AboutUs";
+import AboutUsPage from "./pages/AboutUsPage";
 
 function App() {
   return (
@@ -59,8 +25,8 @@ function App() {
     <Router>
     
       {/* <Header /> */}
-      <DonarDashboardPage/>
-      {/* <AboutUs/> */}
+      {/* <DonarDashboardPage/> */}
+      <AboutUsPage/>
       <div className="App">
         <Routes>
          
